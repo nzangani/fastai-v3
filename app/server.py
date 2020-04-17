6432d8a67c146f08c27f187a627ed15cf81ac45f
nzangani <62846771+nzangani@users.noreply.github.com>
2020-04-17 16:19:57 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1K6FczRcfb1asQCDfPRP4rMv-zM-zGNsE'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['sirsasana','bharadvajasana', "janu_sirshasana", "upavishtha_konasana", 
+           "urdhva_dhanurasana", "sukhasana", "virabhadrasana_2", "adho_mukha_virasana", 
+           "utthita_trikonasana", "savasana", "tadasana", "supta_baddha_konasana"]
 path = Path(__file__).parent
 
 app = Starlette()
